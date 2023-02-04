d9ce67e800b1bbc33fd73055b355f483a380f68f
Nishchay Sinha <nishchay.space@gmail.com>
2023-02-04 19:28:26 +0530

    feat: added basic func for request bodies

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,7 +11,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func goDotEnvVariable(key string) string {
+func loadDotenv(key string) string {
 
 	// load .env file
 	err := godotenv.Load(".env")
@@ -21,22 +23,12 @@ func goDotEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
-func main() {
-
-	dotenv := goDotEnvVariable("BEARER_TOKEN")
-
-	url := "https://api.dropboxapi.com/2/team/groups/list"
-
-	// Create a Bearer string by appending string access token
-	var bearer = "Bearer " + dotenv
-
-	// Create a new request using http
-	req, err := http.NewRequest("POST", url, nil)
-
-	// add authorization header to the req
+func wrapper_emptyBody(token string, method string, endpoint string) {
+	var bearer = "Bearer " + token
+	base_url := "https://api.dropboxapi.com/2/"
+	url := base_url + endpoint
+	req, _ := http.NewRequest(method, url, nil)
 	req.Header.Add("Authorization", bearer)
-
-	// Send req using http Client
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -49,5 +41,44 @@ func main() {
 		log.Println("Error while reading the response bytes:", err)
 	}
 	log.Println(string([]byte(body)))
+
+}
+
+func wrapper_fullBody(token string, method string, endpoint string, reqbody []byte) {
+	var bearer = "Bearer " + token
+
+	base_url := "https://api.dropboxapi.com/2/"
+	url := base_url + endpoint
+	req, _ := http.NewRequest(method, url, bytes.NewBuffer(reqbody))
+	req.Header.Add("Authorization", bearer)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error while reading the response bytes:", err)
+	}
+	log.Println(string([]byte(body)))
+
+}
+
+func main() {
+
+	postBody, _ := json.Marshal(map[string]string{
+		"include_removed": "false",
+		"limit":           "100",
+	})
+
+	dotenv := loadDotenv("BEARER_TOKEN")
+
+	wrapper_emptyBody(dotenv, "POST", "team/groups/list")
+	println("\n")
+	wrapper_emptyBody(dotenv, "POST", "team/get_info")
+	println("\n")
+	wrapper_fullBody(dotenv, "POST", "team/members/list_v2", postBody)
 
 }
